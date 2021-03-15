46fdef8438eacb321ff69b5e2dc0a1ea0088cfb0
steph-mipt <siniakov.se@phystech.edu>
2021-03-15 17:57:52 +0300

    code review fixes

--- 1_contest_hash/a.cpp
+++ 1_contest_hash/a.cpp
@@ -4,48 +4,58 @@
 #include <string>
 #define T_SIZE 100000
 
-long long hash( std::string &str ) {
-    long long ans = 0, x = 1, p = 31;
 
-    for (char i : str)
-    {
-        ans += (x * i);
-        ans %= T_SIZE;
-        x *= p;
-        x %= T_SIZE;
+class hashTable{
+private:
+  std::vector<std::list<std::pair<std::string, int>>> table;
+
+public:
+  hashTable( void ) {
+    table.resize(T_SIZE);
+  }
+
+  long long add( const int val, const std::string &str ) {
+    long long h = hash(str);
+
+    for (auto &i : table[h]) {
+      if (i.first == in) {
+        i.second += val;
+    
+        return i.second;
+      }
     }
+    table[h].push_front({str, val});
+   
+    return val;
+  }
 
-    return ans;
-}
+  long long hash( const std::string &str ) {
+      long long
+           ans = 0,
+           x = 1,
+           p = 31;
+
+      for (char &i : str) {
+          ans += (x * i);
+          ans %= T_SIZE;
+          x *= p;
+          x %= T_SIZE;
+      }
+
+      return ans;
+  }
+};
 
 int main( void ) {
-    std::vector<std::list<std::pair<std::string, int>>> A;
     std::string in;
     int val;
-    bool f = false;
-
-    A.resize(T_SIZE);
+    hashTable hash_table();
 
     while (std::cin >> in) {
         std::cin >> val;
-
-        long long h = hash(in);
-
-        for (auto &i : A[h])
-        {
-           if (i.first == in) {
-               i.second += val;
-               f = true;
-               std::cout << i.second << "\n";
-               break;
-           }
-        }
-        if (!f) {
-            A[h].push_front({in, val});
-            std::cout << val << "\n";
-        }
-        f = false;
+        std::cout << hash_table.add(val, in);
     }
+
     return 0;
 }
 
