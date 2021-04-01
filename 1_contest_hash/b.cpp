7ed4bbf5960f7b417a520cca0dbc0857e02fab20
steph-mipt <siniakov.se@phystech.edu>
2021-04-01 05:45:31 +0300

    code review fixed

--- 1_contest_hash/b.cpp
+++ 1_contest_hash/b.cpp
@@ -4,74 +4,71 @@
 #include <list>
 #include <string>
 
-template<typename T, int SIZE>
+template<typename T>
 class HashTable {
 public:
     int size = 0;
     std::vector<std::list<std::pair<T, T>>> table;
 
-    HashTable( void ) {
+    HashTable( int size = 1000000 ) {
         this->size = SIZE;
         this->table.resize(this->size);
     }
 
     ~HashTable( void ) = default;
 
-    std::pair<T, T> & put( T &x, T &y ) {
+    std::pair<T, T> & put( const T &x, const T &y ) {
         long long h = hash(x);
         bool f = false;
 
-        for (auto &i : table[h])
-        {
-            if (i.first == x) {
-                i.second = y;
-                return i;
+        for (auto &element : table[h]) {
+            if (element.first == x) {
+                element.second = y;
+                return element;
             }
         }
         table[h].push_front({x, y});
         return table[h].front();
     }
 
-    bool pop( T &x ) {
+    bool pop( const T &x ) {
         long long h = hash(x);
 
-        for (auto &i : table[h])
-        {
-            if (i.first == x) {
-                table[h].remove(i);
+        for (auto &element : table[h]) {
+            if (element.first == x) {
+                table[h].remove(element);
                 return true;
             }
         }
         return false;
     }
 
-    T get( T &x ) {
+    T get( const T &x ) {
         long long h = hash(x);
         bool f = false;
 
-        for (auto &i : table[h])
-        {
-            if (i.first == x) {
-                return i.second;
+        for (auto &element : table[h]) {
+            if (element.first == x) {
+                return element.second;
             }
         }
         return 0;
     }
 private:
-    long long hash( T &x ) {
-        long long A = 37, B = 191, p = 3284243;
+    long long hash( const T &x ) {
+        long long constant1 = 37, constant2 = 191, p = 3284243;
 
-        return (A + B * x) % p % this->size;
+        return (constant1 + constant2 * x) % p % this->size;
     }
 public:
-    int swap( T x, T y ) {
+    int swap( const T x, const T y ) {
         long long hx = hash(x), hy = hash(y);
         std::pair<T, T> *tmp1, *tmp2;
         bool f = false;
 
-        for (auto &i : table[hx]) {
-            if (i.first == x) {
-                tmp1 = &i;
+        for (auto &element : table[hx]) {
+            if (element.first == x) {
+                tmp1 = &element;
                 f = true;
                 break;
             }
@@ -102,7 +99,7 @@ int main( void ) {
     std::cin.tie(nullptr);
     std::cout.tie(nullptr);
     int n, ind1, ind2;
-    HashTable<long long, 100000> A;
+    HashTable<long long> A;
 
     std::cin >> n;
 
