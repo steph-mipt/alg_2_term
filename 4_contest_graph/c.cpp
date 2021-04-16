d5e6e6ace9fb35dac4cc8ef678ce11c8475ad90d
steph-mipt <siniakov.se@phystech.edu>
2021-04-16 13:42:20 +0300

    solution for c added

---
+++ 4_contest_graph/c.cpp
@@ -0,0 +1,86 @@
+#include <vector>
+#include <stack>
+#include <algorithm>
+#include <iostream>
+
+class Graph {
+private:
+    std::vector<std::vector<int>> adjancency_list_;
+    int num_vertices_;
+    /// 0 - white, 1 - gray, (-1) - black
+    std::vector<int> colors_;
+    std::stack<int> way;
+
+public:
+    Graph(int num_vertices) : num_vertices_(num_vertices), adjancency_list_(num_vertices) {
+        colors_ = std::vector<int>(num_vertices, 0);
+    }
+
+    void AddEdge(int first, int second) {
+        adjancency_list_[first - 1].push_back(second - 1);
+    }
+
+    void solve(void) {
+        for (int i = 0; i < num_vertices_; ++i) {
+            if (colors_[i] == 0) {
+                if (DFS(i)) {
+                    std::cout << "YES\n";
+
+                    std::vector<int> tmp;
+                    int x = way.top();
+                    way.pop();
+                    tmp.reserve(way.size());
+                    while (way.top() != x) {
+                        tmp.push_back(way.top());
+                        way.pop();
+                    }
+                    tmp.push_back(x);
+                    std::reverse(tmp.begin(), tmp.end());
+                    for (auto vertex : tmp) {
+                        std::cout << vertex + 1 << " ";
+                    }
+                    return;
+                }
+            }
+        }
+        std::cout << "NO";
+    }
+
+    bool DFS(int v) {
+        colors_[v] = 1;
+        for (auto vertex : adjancency_list_[v]) {
+            if (colors_[vertex] == 1) {
+                way.emplace(v);
+                way.emplace(vertex);
+                return true;
+            } else if (colors_[vertex] == -1) {
+                continue;
+            } else {
+                way.emplace(v);
+                if (DFS(vertex))
+                    return true;
+                way.pop();
+            }
+        }
+        colors_[v] = -1;
+        return false;
+    }
+};
+
+int main(void) {
+    int N, M;
+
+    std::cin >> N >> M;
+    Graph g(N);
+    for (int i = 1; i <= M; ++i) {
+        int v1, v2;
+
+        std::cin >> v1 >> v2;
+        g.AddEdge(v1, v2);
+    }
+
+    g.solve();
+
+
+    return 0;
+}
